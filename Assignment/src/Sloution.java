6bf082df656a20f245a593eb98d345d588046128
Ab <abdarker@outlook.com>
2018-12-25 20:15:30 +0600

    Add files via upload

--- Assignment/src/Sloution.java
+++ Assignment/src/Sloution.java
@@ -5,12 +5,14 @@ public class Sloution {
     public static void main(String[] args) {
         Scanner Input= new Scanner(System.in);
         Account a= new Account(Input.next(),Input.nextDouble(),Input.nextDouble(),Input.nextInt(),Input.next());
-
+        double amount= Input.nextDouble();
+        a.changeEmail(Input.next());
         out.println(a.calculatePrincipalWithSimpleInterest());
         out.println(a.calculatePrincipalWithCompoundInterest());
-        a.withdrawMoney(Input.nextDouble());
+
+        a.withdrawMoney(amount);
         out.println(a.getCurrentBalance());
-        a.changeEmail(Input.next());
+
         out.println(a.getEmail());
 
     }
